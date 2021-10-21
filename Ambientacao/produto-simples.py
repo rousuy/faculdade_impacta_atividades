cec1a575aeb551ea1bc6b3764f8cf97e0dbf4cab
Rodrigo Usuy <rousuy@gmail.com>
2021-10-21 15:17:57 -0300

    Incluido resolução de exercícios no Módulo de Ambientação.

---
+++ Ambientacao/produto-simples.py
@@ -0,0 +1,18 @@
+"""
+Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável
+PROD. A seguir mostre a variável PROD com mensagem correspondente.
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da
+igualdade. Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a
+mensagem: “Presentation Error”.
+"""
+
+# Resposta:
+a = int(input())
+b = int(input())
+prod = a * b
+print(f"PROD = {prod}")
