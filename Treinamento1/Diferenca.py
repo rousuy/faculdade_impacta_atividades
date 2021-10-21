4d8cb2cb11f299050f6c74f18e9cab44be3e81c2
Rodrigo Usuy <rousuy@gmail.com>
2021-10-21 10:44:41 -0300

    Criado Módulo de Exercícios

---
+++ Treinamento1/Diferenca.py
@@ -0,0 +1,19 @@
+"""
+Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de
+C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+
+Entrada
+O arquivo de entrada contém 4 valores inteiros.
+
+Saída
+Imprima a mensagem DIFERENCA com todas as letras maiúsculas
+"""
+
+# Resposta:
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+diferenca = (A*B) - (C*D)
+print(f"DIFERENCA = {diferenca}")
