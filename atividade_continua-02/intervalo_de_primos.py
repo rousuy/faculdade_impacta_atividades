93e1179d135b03fdeda7f3efa1163fa1d8ae059f
Rodrigo Usuy <rousuy@gmail.com>
2021-11-02 12:11:03 -0300

    Feito novas resoluções da Atividade AC-02.

---
+++ atividade_continua-02/intervalo_de_primos.py
@@ -0,0 +1,19 @@
+"""
+Os números primos têm diversas aplicações, principalmente na criptografia utilizada pelo aplicativo de seu banco e nos
+sites de compra que nos trazem tanta comodidade.
+
+Um número natural é considerado primo quando possui somente dois divisores, o número um e ele próprio. O número zero
+e o número um não são primos e o número dois é o único primo par.
+
+Seu objetivo é criar um programa que receba como entrada dois números naturais, INÍCIO e FIM, e exibe os números primos
+que existem no intervalo fechado [ INÍCIO..FIM ]. Ao final, o programa também deve exibir a quantidade de primos
+encontrados no intervalo.
+
+Entrada
+Dois números naturais, INÍCIO e FIM, respectivamente, um por linha. Adote (INICIO <= FIM <= 5000).
+
+Saída
+Os números primos presentes no intervalo fechado [ INÍCIO..FIM ] e a quantidade de números primos do intervalo,
+conforme o padrão exibido nos exemplos.
+"""
+# Resposta:
